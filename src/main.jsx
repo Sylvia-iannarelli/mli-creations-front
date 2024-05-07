d92fea76e98782b7934ddf95f9fc4b3f82d76af3
Sylvia-iannarelli <sylvia_iannarelli@hotmail.com>
2024-05-07 18:45:26 +0200

    Correction Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import './index.css'
-import Home from './pages/home/Home.jsx';
-import SingleProduct from './pages/home/SingleProduct.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children: [
-      {
-        path: "/",
-        element: <Home/>
-      },
-      {
-        path: "shop/:id",
-        element: <SingleProduct/>
-      }
-    ]
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from './router/Router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <RouterProvider router={router} />,
 )
